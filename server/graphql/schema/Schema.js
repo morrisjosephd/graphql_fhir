9c2b6e612edc4d29b166391c2995d5e3e5e103bd
Joe Morris <jmorris@pillartechnology.com>
2018-03-28 16:11:44 -0400

    add observations

--- server/graphql/schema/Schema.js
+++ server/graphql/schema/Schema.js
@@ -33,28 +33,28 @@ const Patient = new GraphQLObjectType({
   })
 })
 
-// const Observation = new GraphQLObjectType({
-//   name: 'observation',
-//   description: 'patient observations',
-//   fields: () => ({
-//     type: {
-//       type: GraphQLString,
-//       description: 'Type of observation'
-//     },
-//     date: {
-//       type: Date,
-//       description: 'Date / Time of observation'
-//     },
-//     systolic: {
-//       type: GraphQLInt,
-//       description: 'Systolic blood pressure'
-//     },
-//     diastolic: {
-//       type: GraphQLInt,
-//       description: 'Diastolic blood pressure'
-//     }
-//   })
-// })
+const Observation = new GraphQLObjectType({
+  name: 'observation',
+  description: 'patient observations',
+  fields: () => ({
+    type: {
+      type: GraphQLString,
+      description: 'Type of observation'
+    },
+    date: {
+      type: GraphQLString,
+      description: 'Date / Time of observation'
+    },
+    systolic: {
+      type: GraphQLInt,
+      description: 'Systolic blood pressure'
+    },
+    diastolic: {
+      type: GraphQLInt,
+      description: 'Diastolic blood pressure'
+    }
+  })
+})
 
 module.exports.schema = new GraphQLSchema({
   query: new GraphQLObjectType({
@@ -73,17 +73,29 @@ module.exports.schema = new GraphQLSchema({
         resolve: async (root, args, context, info) => {
           const {firstName, lastName} = args
           let foundPatient = await axios.get(`https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Patient?family=${lastName}&given=${firstName}`)
-          let dog = {}
-          dog.firstName = firstName
-          dog.lastName = lastName
-          dog.fullName = `${firstName} ${lastName}`
-          dog.patientId = foundPatient.data.entry[0].resource.id
+          let patient = {}
+          patient.firstName = firstName
+          patient.lastName = lastName
+          patient.fullName = `${firstName} ${lastName}`
+          patient.patientId = foundPatient.data.entry[0].resource.id
 
-          return dog
+          return patient
+        }
+      },
+      observation: {
+        type: Observation,
+        resolve: async (root, args, context, info) => {
+          let foundObservations = await axios.get(`https://open-ic.epic.com/FHIR/api/FHIR/DSTU2/Observation?patient=Tbt3KuCY0B5PSrJvCu2j-PlK.aiHsu2xUjUM8bWpetXoB&category=vital-signs`)
+          let latestObservation = foundObservations.data.entry[0].resource
+          let observation = {};
+          observation.type = latestObservation.code.text
+          observation.date = latestObservation.effectiveDateTime
+          observation.systolic = latestObservation.component[0].valueQuantity.value
+          observation.diastolic = latestObservation.component[1].valueQuantity.value
+
+          return observation
         }
       }
     }
   })
 })
-
-
